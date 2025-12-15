458902eb7eb77a05628ff0a52369011128381af5
Adib MAROUAN <marouan.adib@alten.com>
2025-12-15 18:33:41 +0100

    binary function

--- basic_math.py
+++ basic_math.py
@@ -2,14 +2,11 @@
 #soustration
 #multiplication
 #division
-<<<<<<< HEAD
-=======
- def divide(a, b):
+def divide(a, b):
         if b != 0:
             print(a / b)
         else:
             print("Cannot divide by zero")
->>>>>>> 4133be3343b57a9a00fb0c842259b6082fd1a8e4
 #modulo
 def modulo(a,b):
     return a % b
